ceffe6706f894420c713b9df9d697c7d081234fd
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2023-03-02 08:42:52 +0100

    import custom font and design of the logo class

--- app.vue
+++ app.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <NuxtWelcome />
+    <p class="logo logo-black">Infline</p>
+    
   </div>
 </template>
